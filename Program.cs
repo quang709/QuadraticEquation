99dd789405e352151e8428a8ffc9fb05446eab3d
quang709 <69231857+quang709@users.noreply.github.com>
2020-10-02 14:16:47 +0700

    QuadraticEquation

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace QuadraticEquationx
+{
+    class Program
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.OutputEncoding = Encoding.UTF8;
+                Console.WriteLine("Nhập a: ");
+                double a = double.Parse(Console.ReadLine());
+                Console.WriteLine("Nhập b: ");
+                double b = double.Parse(Console.ReadLine());
+                Console.WriteLine("Nhập c: ");
+                double c = double.Parse(Console.ReadLine());
+                QuadraticEquation qe = new QuadraticEquation(a, b, c);
+
+                if (qe.GetDiscriminant() > 0)
+                {
+                    Console.WriteLine("Phương trình có 2 nghiệm x1 = {0}, x2 = {1}: ", qe.GetRoot2x1(), qe.GetRoot2x2());
+                }
+                else if (qe.GetDiscriminant() == 0)
+                {
+                    Console.WriteLine("Phương trình có một nghiệm duy nhất: " + qe.GetRoot1());
+                }
+                else
+                    Console.WriteLine("Phương trình vô nghiệm");
+            }
+        }
+    }
